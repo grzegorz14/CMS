3dcc91ac71854346c60b530b48626ffd543cec1f
Grzegorz Szewczyk <g.szewczyk196@gmail.com>
2022-05-02 00:11:56 +0200

    Log in handling and Header changes

--- client/src/lib/Footer.svelte
+++ client/src/lib/Footer.svelte
@@ -1,4 +1,4 @@
 <footer class="container">
-    <p class="float-end"><a href="/">Back to top</a></p>
-    <p>© 2022–2022 Grzegorz Szewczyk & Mikołaj Zając · <a href="/">Privacy</a> · <a href="/">Terms</a></p>
+    <p class="float-end"><a href={"#"}>Back to home</a></p>
+    <p>© 2022–2022 Grzegorz Szewczyk & Mikołaj Zając · <a href={"#"}>Privacy</a> · <a href={"#"}>Terms</a></p>
 </footer>
--- client/src/lib/LogIn.svelte
+++ client/src/lib/LogIn.svelte
@@ -4,6 +4,41 @@
     function goHome() {
         push("#/");
     }
+
+    let errorInfo = "Login and password are required."
+
+    async function logIn() {
+        let login = document.getElementById("loginInput").value
+        let password = document.getElementById("passwordInput").value
+        if (isEmptyOrWhiteSpace(login) || isEmptyOrWhiteSpace(password)){
+            document.getElementById("errorInfo").classList.remove("d-none")
+            document.getElementById("passwordBox").classList.remove("mb-4")
+        }
+        else {
+            let response = await fetch("./logIn", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams({
+                    "login": login,
+                    "password": password,
+                }),
+            })
+            response = await response.text()
+            if (response == "admin" || response == "user") {
+                //store somewhere user type info
+                push("#/")
+            }
+            else {
+                errorInfo = response
+            }
+        }
+    }
+
+    function isEmptyOrWhiteSpace(str){
+        return str === null || str.match(/^ *$/) !== null
+    }
 </script>
 
 <button on:click={goHome} class="goBack btn btn-outline-light position-absolute">HOME</button>
@@ -13,29 +48,28 @@
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                 <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                    <div class="card-body p-5 text-center">
+                    <div action="/logIn" method="post" class="card-body p-5 text-center">
+                        <h2 class="fw-bold mb-2 text-uppercase mb-5">Log in</h2>
 
-                    <h2 class="fw-bold mb-2 text-uppercase mb-5">Log in</h2>
+                        <div class="form-outline form-white mb-4">
+                            <input type="text" id="loginInput" class="form-control form-control-lg" />
+                            <label class="form-label mt-1" for="loginInput">Login</label>
+                        </div>
 
-                    <div class="form-outline form-white mb-4">
-                    <input type="text" id="loginInput" class="form-control form-control-lg" />
-                    <label class="form-label mt-1" for="loginInput">Login</label>
-                    </div>
+                        <div id="passwordBox" class="form-outline form-white mb-4">
+                            <input type="password" id="passwordInput" class="form-control form-control-lg" />
+                            <label class="form-label mt-1" for="passwordInput">Password</label>
+                        </div>
 
-                    <div class="form-outline form-white mb-4">
-                    <input type="password" id="passwordInput" class="form-control form-control-lg" />
-                    <label class="form-label mt-1" for="passwordInput">Password</label>
-                    </div>
+                        <p id="errorInfo" class="form-label text-danger mb-4 d-none">{errorInfo}</p>
+                        <button on:click={logIn} class="btn btn-outline-light btn-lg px-5 mb-4">Log in</button>
 
-                    <button class="btn btn-outline-light btn-lg px-5 mb-4" type="submit">Log in</button>
-
-                    <div>
-                        <p class="mb-0">
-                            Don't have an account? 
-                            <a href="/#/signUp" class="text-white-50 fw-bold">Sign Up</a>
-                        </p>
-                    </div>
-
+                        <div>
+                            <p class="mb-0">
+                                Don't have an account? 
+                                <a href="/#/signUp" class="text-white-50 fw-bold">Sign Up</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
