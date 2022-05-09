afaae86038cc009224394446c7d8a273cddc4915
drater1 <mikolajzv@gmail.com>
2022-05-09 18:55:57 +0200

    ..

--- client/src/lib/components/Articles.svelte
+++ client/src/lib/components/Articles.svelte
@@ -8,7 +8,7 @@
     <div class="col mb-5" style="max-width: 50%; max-height: 250px">
       <h3 class="mb-3">Air pollution</h3>
       <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-      <p><a class="btn {colorTheme == "Light" ? 'bg-cyan-200 hover:bg-cyan-100 text-black':  ($colorTheme == "Dark" ? 'bg-indygo-500 hover:bg-indygo-400 text-white' : "bg-yellow-500 hover:bg-yellow-300 text-white")}" href="/#/subPage/article" role="button">Read article »</a></p>
+      <p><a class="btn {colorTheme == "Light" ? 'bg-cyan-200 hover:bg-cyan-100 text-black':  (colorTheme == "Dark" ? 'bg-indygo-500 hover:bg-indygo-400 text-white' : "bg-yellow-500 hover:bg-yellow-300 text-white")}" href="/#/subPage/article" role="button">Read article »</a></p>
     </div>
     <div class="col mb-5" style="max-width: 50%; max-height: 250px;">
       <h3 class="mb-3">Phases of the moon</h3>
