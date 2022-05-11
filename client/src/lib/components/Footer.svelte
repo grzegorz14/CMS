03ae437af7b755b3ec3f7745d99690aebcb81cd2
Grzegorz Szewczyk <g.szewczyk196@gmail.com>
2022-05-11 16:16:16 +0200

    Footer fix

--- client/src/lib/components/Footer.svelte
+++ client/src/lib/components/Footer.svelte
@@ -4,7 +4,7 @@
     let colorTheme = settings.getJson().colorTheme
 </script>
 
-<footer class="container {colorTheme == "Light" ? 'bg-white t-black' :  (colorTheme == "Dark" ? 'bg-dark t-white' : "bg-black t-yellow")}">
+<footer class="container p-2 {colorTheme == "Light" ? 'bg-white t-black' :  (colorTheme == "Dark" ? 'bg-dark t-white' : "bg-black t-yellow")}">
     <p class="float-end"><a href={"#"}>Back to home</a></p>
     <p>© 2022–2022 Grzegorz Szewczyk & Mikołaj Zając · <a href={"#"}>Privacy</a> · <a href={"#"}>Terms</a></p>
 </footer>
